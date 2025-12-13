aa800ae064545d0dd8db7fa0605c5f52bbd568a8
SadaqElmi <sadaqelmi.dev@gmail.com>
2025-12-13 15:10:39 +0300

    Add navbar

---
+++ src/components/Home/NavBar.tsx
@@ -0,0 +1,15 @@
+import "./navbar.css";
+function NavBar() {
+  return (
+    <div>
+      <h1 className="navbar-title">NavBar</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
